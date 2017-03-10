23f498b2b6beff60d5d332ee74f7e7b7279efb25
whitehat-zero <docdef286@gmail.com>
2017-03-10 17:51:45 +0800

    Added Modes and Functions

--- PowEnum.ps1
+++ PowEnum.ps1
@@ -1,21 +1,42 @@
+function Invoke-PowEnum
+{
 <# 
 .SYNOPSIS 
-    Quickly enumerate local domain using PowerSploit's PowerView and combine into XLSX
+    Quickly enumerate domain info using PowerSploit's PowerView and combine into XLSX
 .DESCRIPTION 
-    Learning powershell scripting, and this came about.
-	Its not full blown script yet (with parameters and such).
-	Credit goes to contributers of PowerView
+    I've been teaching myself PowerShell scripting and this came about.
+	Credit goes to contributers of PowerView for making a great tool.
 .NOTES 
+	Requires Excel to be installed on the systems running this script.
+	TODO:
+		Mode parameter (1-OnlyDCEnum 2-OnlyEnum 3-UserHunting 4-Kerberoast 5-LargeEnv)
+		Sheets (1-User/GroupEnumeration 2-Computer/SessionEnumeration 3-Kerberoast/AS-REPoast)
+			Add speciality user enum to user xls (http://www.netvision.com/ad_useraccountcontrol.php)
+			Create sperate xls for for invokes (Invoke-Kerb, Find-DomainLocalGroupMem, Find-DomainUserLocation -Stealth)
+	
 .LINK 
 	PowerSploit PowerView
-	https://github.com/PowerShellMafia/PowerSploit/blob/master/Recon/PowerView.ps1
+	https://github.com/PowerShellMafia/PowerSploit/blob/dev/Recon/PowerView.ps1
 	
 	Export to CSV
 	https://gist.github.com/gregklee/b01348787af0b47d8b30
 .EXAMPLE 
-    iex('.\PowEnum.ps1'}
+    Invoke-PowEnum -Domain test.com
 #>
 
+[CmdletBinding(DefaultParameterSetName="Domain")]
+Param(
+	[Parameter(Position = 0)]
+	[String]
+	$Domain,
+	
+	[Parameter(Position = 1)]
+	[ValidateSet('DCOnly', 'Hunting', 'Kerberoast', 'LargeEnv')]
+    [String]
+    $Mode = 'DCOnly'
+)
+	
+	
 Write-Host "To run from a non-domain joined system:" -ForegroundColor Cyan
 Write-Host "runas /netonly /user:DOMAIN\USERNAME powershell.exe"
 
@@ -30,86 +51,157 @@ Write-Host "Downloading Powerview:" -ForegroundColor Cyan
 Write-Host "$url"
 IEX $webclient.DownloadString($url)
 
-#Grab Local Domain Using PowerView Function
-$domain = Get-Domain | Select-Object -ExpandProperty Name
-Write-Host "Starting Enumeration: $domain" -ForegroundColor Cyan
+#Grab Local Domain Using PowerView Function If None Provided
+if (!$domain) {$domain = (Get-Domain).Name}
+Write-Host "Enumeration Domain: $domain" -ForegroundColor Cyan
 
-Write-Host "[ ]Domain Controllers | " -NoNewLine
-$temp = Get-DomainController -Domain $domain 
-if($temp -ne $null){$temp | Export-CSV -NoTypeInformation .\01_DCs.csv}
-$count = $temp | measure-object | select-object -expandproperty Count
-Write-Host "$count Identified" -ForegroundColor Green
+$ErrorActionPreference = 'Continue'
+$CSVSheetCount = 1
 
-Write-Host "[ ]Domain Admins | " -NoNewLine
-$temp = Get-DomainGroupMember -Identity "Domain Admins" -Domain $domain
-if($temp -ne $null){$temp | Export-CSV -NoTypeInformation .\02_DAs.csv}
-$count = $temp | measure-object | select-object -expandproperty Count
-Write-Host "$count Identified" -ForegroundColor Green
+if ($Mode -eq 'DCOnly') {
+	Write-Host "Enumeration Mode: $Mode" -ForegroundColor Cyan
+	PowEnum-DCs
+	PowEnum-DAs
+	PowEnum-EAs
+	PowEnum-BltAdmins
+	PowEnum-Users
+	PowEnum-Groups
+	PowEnum-Computers
+	PowEnum-IPs
+	PowEnum-DCLocalAdmins
+	PowEnum-Subnets
+	PowEnum-DNSRecords
+	PowEnum-HVTs
+	PowEnum-NetSess
+}
+elseif ($Mode -eq 'Hunting') {
 
-Write-Host "[ ]Enterprise Admins | " -NoNewLine
-$temp = Get-DomainGroupMember -Identity "Enterprise Admins" -Domain $domain
-if($temp -ne $null){$temp | Export-CSV -NoTypeInformation .\03_EAs.csv}
-$count = $temp | measure-object | select-object -expandproperty Count
-Write-Host "$count Identified" -ForegroundColor Green
+}
+elseif ($Mode -eq 'Kerberoast') {
+}
+elseif ($Mode -eq 'LargeEnv') {
+}
+else {
+	Write-Host "Incorrect Mode Selected"
+	Return
+}
 
-Write-Host "[ ]Builtin Administrators | " -NoNewLine
-$temp = Get-DomainGroupMember -Identity "Administrators" -Domain $domain
-if($temp -ne $null){$temp | Export-CSV -NoTypeInformation .\04_BltAdmins.csv}
-$count = $temp | measure-object | select-object -expandproperty Count
-Write-Host "$count Identified" -ForegroundColor Green
 
-Write-Host "[ ]All Domain Users | " -NoNewLine
-$temp = Get-DomainUser -Domain $domain
-if($temp -ne $null){$temp | Export-CSV -NoTypeInformation .\05_Users.csv}
-$count = $temp | measure-object | select-object -expandproperty Count
-Write-Host "$count Identified" -ForegroundColor Green
 
-Write-Host "[ ]All Domain Groups | " -NoNewLine
-$temp = Get-DomainGroup -Domain $domain
-if($temp -ne $null){$temp | Export-CSV -NoTypeInformation .\06_Groups.csv}
-$count = $temp | measure-object | select-object -expandproperty Count
-Write-Host "$count Identified" -ForegroundColor Green
+PowEnum-ExcelFile -SpreadsheetName AllLocalEnumeration
 
-Write-Host "[ ]All Domain Computers | " -NoNewLine
-$temp = Get-NetComputer -Domain $domain
-if($temp -ne $null){$temp | Export-CSV -NoTypeInformation .\07_Computers.csv}
-$count = $temp | measure-object | select-object -expandproperty Count
-Write-Host "$count Identified" -ForegroundColor Green
+$stopwatch.Stop()
+Write-Host "Running Time: $($stopwatch.Elapsed.TotalSeconds) seconds"
+Write-Host "Exiting..." -ForegroundColor Yellow
+}
 
-Write-Host "[ ]All Domain Computer IP Addresses  | " -NoNewLine
-$temp = Get-DomainComputer -Domain $domain | Get-IPAddress
-if($temp -ne $null){$temp | Export-CSV -NoTypeInformation .\08_IPs.csv}
-$count = $temp | measure-object | select-object -expandproperty Count
-Write-Host "$count Identified" -ForegroundColor Green
 
-Write-Host "[ ]All Domain Controller Local Admins | " -NoNewLine
-$temp = Get-DomainController -Domain $domain | Get-NetLocalGroupMember
-if($temp -ne $null){$temp | Export-CSV -NoTypeInformation .\09_DCLocalAdmins.csv}
-$count = $temp | measure-object | select-object -expandproperty Count
-Write-Host "$count Identified" -ForegroundColor Green
+function PowEnum-DCs {
+	Write-Host "[ ]Domain Controllers | " -NoNewLine
+	$temp = Get-DomainController -Domain $domain 
+	PowEnum-ExportAndCount -TypeEnum DCs
+}
 
-Write-Host "[ ]All Domain Subnets | " -NoNewLine
-$temp = Get-DomainSubnet -Domain $domain
-if($temp -ne $null){$temp | Export-CSV -NoTypeInformation .\10_Subnets.csv}
-$count = $temp | measure-object | select-object -expandproperty Count
-Write-Host "$count Identified" -ForegroundColor Green
+function PowEnum-DAs {
+	Write-Host "[ ]Domain Admins | " -NoNewLine
+	$temp = Get-DomainGroupMember -Identity "Domain Admins" -Domain $domain
+	PowEnum-ExportAndCount -TypeEnum DAs
+}
 
-Write-Host "[ ]All DNS Zones & Records | " -NoNewLine
-$temp = Get-DomainDNSZone -Domain $domain | Get-DomainDNSRecord
-if($temp -ne $null){$temp | Export-CSV -NoTypeInformation .\11_DNSRecords.csv}
-$count = $temp | measure-object | select-object -expandproperty Count
-Write-Host "$count Identified" -ForegroundColor Green
+function PowEnum-EAs {
+	Write-Host "[ ]Enterprise Admins | " -NoNewLine
+	$temp = Get-DomainGroupMember -Identity "Enterprise Admins" -Domain $domain
+	PowEnum-ExportAndCount -TypeEnum EAs
+}
 
-Write-Host "[ ]All High Value Targets | " -NoNewLine
-$temp = Get-DomainController -Domain $domain | Get-NetLocalGroupMember | Select-Object -ExpandProperty MemberName | %{$_ -replace '^[^\\]*\\', ''} | Get-DomainGroupMember -Recurse
-if($temp -ne $null){$temp | Export-CSV -NoTypeInformation .\12_HVTs.csv}
-$count = $temp | measure-object | select-object -expandproperty Count
-Write-Host "$count Identified" -ForegroundColor Green
+function PowEnum-BltAdmins {
+	Write-Host "[ ]Builtin Administrators | " -NoNewLine
+	$temp = Get-DomainGroupMember -Identity "Administrators" -Domain $domain
+	PowEnum-ExportAndCount -TypeEnum BltAdmins
+}
 
-Invoke-Command -ScriptBlock {
+function PowEnum-Users {
+	Write-Host "[ ]All Domain Users | " -NoNewLine
+	$temp = Get-DomainUser -Domain $domain
+	PowEnum-ExportAndCount -TypeEnum Users
+}
+
+function PowEnum-Groups {
+	Write-Host "[ ]All Domain Groups | " -NoNewLine
+	$temp = Get-DomainGroup -Domain $domain
+	PowEnum-ExportAndCount -TypeEnum Groups
+}
+
+function PowEnum-Computers {
+	Write-Host "[ ]All Domain Computers | " -NoNewLine
+	$temp = Get-NetComputer -Domain $domain
+	PowEnum-ExportAndCount -TypeEnum Computers
+}
+
+function PowEnum-IPs {
+	Write-Host "[ ]All Domain Computer IP Addresses  | " -NoNewLine
+	$temp = Get-DomainComputer -Domain $domain | Get-IPAddress
+	PowEnum-ExportAndCount -TypeEnum IPs
+}
+
+function PowEnum-DCLocalAdmins {
+	Write-Host "[ ]All Domain Controller Local Admins | " -NoNewLine
+	$temp = Get-DomainController -Domain $domain | Get-NetLocalGroupMember
+	PowEnum-ExportAndCount -TypeEnum DCLocalAdmins
+}
+
+function PowEnum-Subnets {
+	Write-Host "[ ]Domain Subnets | " -NoNewLine
+	$temp = Get-DomainSubnet -Domain $domain
+	PowEnum-ExportAndCount -TypeEnum Subnets
+}
+
+function PowEnum-DNSRecords {
+	Write-Host "[ ]DNS Zones & Records | " -NoNewLine
+	$temp = Get-DomainDNSZone -Domain $domain | Get-DomainDNSRecord
+	PowEnum-ExportAndCount -TypeEnum DNSRecords
+}
+
+function PowEnum-HVTs {
+	Write-Host "[ ]High Value Targets | " -NoNewLine
+	$temp = Get-DomainController -Domain $domain | Get-NetLocalGroupMember | Select-Object -ExpandProperty MemberName | %{$_ -replace '^[^\\]*\\', ''} | Get-DomainGroupMember -Recurse
+	PowEnum-ExportAndCount -TypeEnum HVTs
+}
+
+function PowEnum-NetSess {
+	try {
+	Write-Host "[ ]Net Sessions | " -NoNewLine
+	$temp = Get-DomainController -Domain $domain | Get-NetSession -ErrorAction SilentlyContinue
+	PowEnum-ExportAndCount -TypeEnum NetSess
+	}
+	catch {
+	Write-Host "Error"
+	}
+}
+
+function PowEnum-ExportAndCount {
+	Param(
+		[Parameter(Position = 0)]
+		[String]
+		$TypeEnum
+	)
+	if($temp -ne $null){$temp | Export-CSV -NoTypeInformation -Path ('.\' + $CSVSheetCount +'_' + $TypeEnum + '.csv')}
+	$count = $temp | measure-object | select-object -expandproperty Count
+	Write-Host "$count Identified" -ForegroundColor Green
+	$CSVSheetCount++
+}
+
+function PowEnum-ExcelFile
+{
+	Param(
+		[Parameter(Position = 0, Mandatory = $True)]
+		[String]
+		$SpreadsheetName
+	)
+	
 	Write-Host "[ ]Combining CSV Files to XSLX | " -NoNewLine
 	$path = (Get-Item -Path ".\" -Verbose).FullName
-	$XLOutput =  $path + "\" + $env:USERNAME + "_AllLocalEnumeration" + "_" + $(get-random) + ".xlsx"
+	$XLOutput =  $path + "\" + $env:USERNAME + "_$SpreadsheetName" + "_" + $(get-random) + ".xlsx"
 
 	$csvFiles = Get-ChildItem (".\*") -Include *.csv | Sort-Object | Where {$_.Length -ne 0}
 
@@ -165,7 +257,3 @@ Invoke-Command -ScriptBlock {
 	[System.GC]::Collect()
 	[System.GC]::WaitForPendingFinalizers()
 }
-
-$stopwatch.Stop()
-Write-Host "Running Time: $($stopwatch.Elapsed.TotalSeconds) seconds"
-Write-Host "Exiting..." -ForegroundColor Yellow
